efee498b31b1bdf81680bc970635008ed0f4023e
JosephW143 <josephwright1436@gmail.com>
2022-11-09 23:36:16 -0500

    reconfig server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
+
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build'));
+});
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+const startApolloServer = async(typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+}
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -18,4 +37,8 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+  console.log(`Use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);
 });
+
+startApolloServer(typeDefs, resolvers);
