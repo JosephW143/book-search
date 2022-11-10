ab4ff24ac5f256e810545404c69c53dc319b1c91
JosephW143 <josephwright1436@gmail.com>
2022-11-10 02:46:00 -0500

    update login form

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -24,7 +24,7 @@ const SignupForm = () => {
 
     try {
       const { data } = await addUser({
-        variables: {...userFormData}
+        variables: { ...userFormData }
       });
     
       Auth.login(data.addUser.token);
